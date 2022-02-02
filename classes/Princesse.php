acac6586e377b1da81f97a002b3e415d52d88486
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-02-02 11:29:40 +0100

    fini

---
+++ classes/Princesse.php
@@ -0,0 +1,14 @@
+<?php
+
+class Princesse extends Personnage
+{
+    private int $save;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->save = 0;
+        $this->x = 450;
+        $this->y = 450;
+    }
+}
